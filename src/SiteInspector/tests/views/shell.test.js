966122873a55da60bcc7f35c3faf977ce3334776
Ryan Rife <ryrife@microsoft.com>
2018-08-30 14:32:04 -0700

    Added copyright, updated snapshot

--- src/SiteInspector/tests/views/shell.test.js
+++ src/SiteInspector/tests/views/shell.test.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import React from 'react';
 import configureStore from 'redux-mock-store';
 import { configure, shallow } from 'enzyme';
