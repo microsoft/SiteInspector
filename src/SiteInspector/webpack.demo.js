92ccd435ca9763de0a358b493ec2ec81eeb20665
Ryan Rife <ryrife@microsoft.com>
2018-11-15 17:52:55 -0800

    Fixed tab license, lint error

--- src/SiteInspector/webpack.demo.js
+++ src/SiteInspector/webpack.demo.js
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import merge from 'webpack-merge';
 import path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import devConfig from './webpack.dev';
 
