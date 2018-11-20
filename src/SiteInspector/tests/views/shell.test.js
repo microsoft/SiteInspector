dcb5b1a1288f52e6a09880da88e4b8060175ac62
Ryan Rife <ryrife@microsoft.com>
2018-11-20 15:01:30 -0800

    Shell unit tests

--- src/SiteInspector/tests/views/shell.test.js
+++ src/SiteInspector/tests/views/shell.test.js
@@ -6,8 +6,9 @@ import configureStore from 'redux-mock-store';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ShellContainer from '../../views/Shell/container';
+import config from '../../shared/config';
 
-describe('Shell view tests', () => {
+describe('Primary container view tests', () => {
   const initialState = {
     ShellState: {
       currentPath: '',
@@ -22,20 +23,80 @@ describe('Shell view tests', () => {
   };
   const mockStore = configureStore();
   let store;
+  let container;
   let shell;
 
   configure({ adapter: new Adapter() });
 
   beforeEach(() => {
+    // suppress react import warning
+    console.error = jest.fn();
+
+    config.title = 'SI Test';
+
     store = mockStore(initialState);
-    shell = shallow(<ShellContainer store={store} />);
+    container = shallow(<ShellContainer store={store} />);
   });
 
   it('Container was created', () => {
-    expect(shell.length).toEqual(1);
+    expect(container.length).toEqual(1);
   });
 
   it('SiteInspector rendered properly', () => {
-    expect(shell).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
+  });
+
+  describe('Shell component tests', () => {
+    beforeEach(() => {
+      store = mockStore(initialState);
+      container = shallow(<ShellContainer store={store} />);
+      shell = container.dive();
+    });
+
+    it('Shell is created', () => {
+      expect(shell.length).toEqual(1);
+    });
+
+    it('Tab is rendered', () => {
+      expect(shell.find('.shell-client-tab').length).toEqual(1);
+    });
+
+    it('Panel is rendered', () => {
+      const panel = shell.find('.panel');
+      expect(panel.length).toEqual(1);
+      expect(panel.hasClass('visible')).toBeFalsy();
+      expect(panel).toMatchSnapshot();
+    });
+
+    it('Panel visbility is toggled after click', () => {
+      const panel = shell.find('.panel');
+      shell.find('.shell-client-tab').simulate('click');
+      setTimeout(() => {
+        expect(panel.hasClass('visible')).toBeTruthy();
+        shell.find('.shell-client-tab').simulate('click');
+        setTimeout(() => {
+          expect(panel.hasClass('visible')).toBeFalsy();
+        }, 1500);
+      }, 1500);
+    });
+
+    it('Panel has router', () => {
+      const panel = shell.find('.panel');
+      expect(panel.find('ShellInspectorRouter').length).toEqual(1);
+    });
+
+    it('Panel has menu', () => {
+      const panel = shell.find('.panel');
+      expect(panel.find('.si-header-right').length).toEqual(1);
+      const menu = panel.find('.si-header-right');
+      expect(menu.find('ActionMenu').length).toEqual(1);
+    });
+
+    it('Panel has correct title', () => {
+      const panel = shell.find('.panel');
+      const title = panel.find('.si-header-title');
+      expect(title.length).toEqual(1);
+      expect(title.text()).toEqual('SI Test');
+    })
   });
 });
