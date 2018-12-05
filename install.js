675ee94f0683ab060485f3ae7462fa066587b1ec
Joshua Whatley <joshwha@microsoft.com>
2018-12-04 16:27:29 -0800

    Added script to remove npm files added by --prefix

---
+++ install.js
@@ -0,0 +1,47 @@
+const { execSync } = require('child_process');
+const { readdirSync, statSync, unlinkSync } = require('fs');
+const { join } = require('path');
+
+const tabsRoot = 'src/Tabs/';
+const siteInspectorClientDirectory = 'src/SiteInspector';
+
+const installDirectories = readdirSync(tabsRoot)
+  .filter(folder =>
+    folder !== 'dist' &&
+    statSync(join(tabsRoot, folder)).isDirectory())
+  .map(folder => tabsRoot + folder)
+
+installDirectories.push(siteInspectorClientDirectory);
+
+installDirectories.forEach((directory) => {
+  const existingFiles = readdirSync(directory)
+    .filter(file => !statSync(join(directory, file)).isDirectory());
+
+  const npmInstallCommand = `npm --prefix ${directory} install ${directory}`;
+
+  console.log(`Running ${npmInstallCommand}`);
+
+  try {
+    execSync(npmInstallCommand,
+      (error, stdout) => {
+        if (stdout) {
+          console.log(stdout);
+        }
+
+        if (error === null) {
+          console.log(`Install succeeded for ${directory}`);
+        } else {
+          console.log(console.log(error));
+        }
+      });
+  } catch (ex) {
+    console.log(ex);
+  }
+
+  console.log('Removing npm --prefix files');
+
+  readdirSync(directory)
+    .filter(file =>
+      !statSync(join(directory, file)).isDirectory() && !existingFiles.includes(file))
+    .forEach(file => unlinkSync(join(directory, file)));
+});
